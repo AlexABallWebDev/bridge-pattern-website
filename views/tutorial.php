db7ae03f2823a84f57856d13b75b7b506078f15e
Alex Ball <aball6@mail.greenriver.edu>
2016-11-26 18:58:33 -0800

    Added Bootstrap. Set up basic design of the website.

--- views/tutorial.php
+++ views/tutorial.php
@@ -1,10 +1,52 @@
 <!doctype html>
-<html>
+<html lang="en">
 <head>
   <meta charset="utf-8" />
+  <meta http-equiv="X-UA-Compatible" content="IE=edge" />
+  <meta name="viewport" content="width=device-width, initial-scale=1" />
+
   <title>{{ @title }}</title>
-</head>
-<body>
-  <h1>{{ @title }}</h1>
+
+  <!-- Bootstrap css -->
+  <link rel="stylesheet"
+        href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css"
+        integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u"
+        crossorigin="anonymous" />
+
+  <!-- Bridge Pattern website css -->
+  <link rel="stylesheet"
+        href="css/styles.css" />
+
+    <!-- HTML5 shim and Respond.js for IE8 support of HTML5 elements and media queries -->
+    <!--[if lt IE 9]>
+      <script src="https://oss.maxcdn.com/html5shiv/3.7.3/html5shiv.min.js"></script>
+      <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
+    <![endif]-->
+  </head>
+  <body>
+    <!-- Header -->
+    <nav class="navbar navbar-inverse navbar-fixed-top header">
+      <div class="container">
+        <div class="navbar-header">
+          <h1>{{ @title }}</h1>
+        </div>
+      </div>
+    </nav>
+
+    <!-- Tutorial Content -->
+    <div class="container tutorial">
+      <div>
+        <p>
+          This page describes the Bridge Pattern, a Design Pattern in the
+          world of Software Development.
+        </p>
+      </div>
+    </div>
+
+    <!-- jQuery (necessary for Bootstrap's JavaScript plugins) -->
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js"></script>
+
+    <!-- Bootstrap js -->
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js" integrity="sha384-Tc5IQib027qvyjSMfHjOMaLkfuWVxZxUPnCJA7l2mCWNIpG9mGCD8wGNIcPD7Txa" crossorigin="anonymous"></script>
 </body>
 </html>
